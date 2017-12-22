ff10dfd43b73873545a2e40c68d73ab7ca76944c
mandy6720 <amanda.hewitt01@gmail.com>
2017-12-22 11:43:57 -0500

    Adds canvas resize function on init

--- app/app.js
+++ app/app.js
@@ -2,9 +2,59 @@ import QuickSettings from 'quicksettings';
 import _ from 'lodash';
 
 var App = {
+  // elems
+	rootElem : null,
+	canvasElem : null,
+  hiResCanvasElem : null,
+  svgElem : null,
+
+  // document data
+	documentData : {
+		workspaceSize : {x: 0, y: 0},
+		backgroundImageSize : {x: 0, y: 0},
+		backgroundImage : new Image(),
+		grids : [],
+		magnets : [],
+		masks : [],
+  },
+
+  // other state
+	canvasContext : null,
+  hiResContext : null,
+  hiResScale : 4,
+  isShowingSettings : true,
+  isShowingMagnets : true,
+  isShowingMasks : true,
+
   init(rootElem) {
     var canvasElem = document.getElementById("app-canvas");
-    console.log('INIT!!')
+    this.canvasElem = canvasElem;
+		this.canvasContext = canvasElem.getContext('2d');
+    this.hiResCanvasElem = document.createElement("canvas");
+    this.hiResContext = this.hiResCanvasElem.getContext('2d');
+    this.rootElem = rootElem;
+    this.svgElem = document.getElementById("svg-workspace");
+
+    this.hiResCanvasElem.setAttribute("style", "position: absolute; top: 0; left: 0; display: none;");
+    rootElem.appendChild(this.hiResCanvasElem);
+
+    QuickSettings.useExtStyleSheet();
+
+		this.fitCanvasToWindow();
+  },
+
+  fitCanvasToWindow() {
+    this.canvasElem.width = this.rootElem.clientWidth;
+    this.canvasElem.height = this.rootElem.clientHeight;
+
+    this.hiResCanvasElem.width = this.rootElem.clientWidth * this.hiResScale;
+    this.hiResCanvasElem.height = this.rootElem.clientHeight * this.hiResScale;
+
+    this.documentData.workspaceSize.x = this.canvasElem.width;
+    this.documentData.workspaceSize.y = this.canvasElem.height;
+
+    this.svgElem.setAttribute('width', this.canvasElem.width);
+    this.svgElem.setAttribute('height', this.canvasElem.height);
   }
 }
 
--- public/app.js
+++ public/app.js
@@ -166,9 +166,58 @@ var _lodash2 = _interopRequireDefault(_lodash);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var App = {
+  // elems
+  rootElem: null,
+  canvasElem: null,
+  hiResCanvasElem: null,
+  svgElem: null,
+
+  // document data
+  documentData: {
+    workspaceSize: { x: 0, y: 0 },
+    backgroundImageSize: { x: 0, y: 0 },
+    backgroundImage: new Image(),
+    grids: [],
+    magnets: [],
+    masks: []
+  },
+
+  // other state
+  canvasContext: null,
+  hiResContext: null,
+  hiResScale: 4,
+  isShowingSettings: true,
+  isShowingMagnets: true,
+  isShowingMasks: true,
+
   init: function init(rootElem) {
     var canvasElem = document.getElementById("app-canvas");
-    console.log('INIT!!');
+    this.canvasElem = canvasElem;
+    this.canvasContext = canvasElem.getContext('2d');
+    this.hiResCanvasElem = document.createElement("canvas");
+    this.hiResContext = this.hiResCanvasElem.getContext('2d');
+    this.rootElem = rootElem;
+    this.svgElem = document.getElementById("svg-workspace");
+
+    this.hiResCanvasElem.setAttribute("style", "position: absolute; top: 0; left: 0; display: none;");
+    rootElem.appendChild(this.hiResCanvasElem);
+
+    _quicksettings2.default.useExtStyleSheet();
+
+    this.fitCanvasToWindow();
+  },
+  fitCanvasToWindow: function fitCanvasToWindow() {
+    this.canvasElem.width = this.rootElem.clientWidth;
+    this.canvasElem.height = this.rootElem.clientHeight;
+
+    this.hiResCanvasElem.width = this.rootElem.clientWidth * this.hiResScale;
+    this.hiResCanvasElem.height = this.rootElem.clientHeight * this.hiResScale;
+
+    this.documentData.workspaceSize.x = this.canvasElem.width;
+    this.documentData.workspaceSize.y = this.canvasElem.height;
+
+    this.svgElem.setAttribute('width', this.canvasElem.width);
+    this.svgElem.setAttribute('height', this.canvasElem.height);
   }
 };
 
