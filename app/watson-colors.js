c3a5cdd8355ceca97ca7a1ee92bbe854c413b81e
mandy6720 <amanda.hewitt01@gmail.com>
2017-12-22 11:36:08 -0500

    imports styles from level 3

---
+++ app/watson-colors.js
@@ -0,0 +1,582 @@
+export default [
+    {
+        "swatches": [
+            {
+                "name": "Purple 10",
+                "color": "#eed2ff"
+            },
+            {
+                "name": "Purple 20",
+                "color": "#d7aaff"
+            },
+            {
+                "name": "Purple 30",
+                "color": "#ba8ff7"
+            },
+            {
+                "name": "Purple 40",
+                "color": "#af6ee8"
+            },
+            {
+                "name": "Purple 50",
+                "color": "#9855d4"
+            },
+            {
+                "name": "Purple 60",
+                "color": "#734098"
+            },
+            {
+                "name": "Purple 70",
+                "color": "#562f72"
+            },
+            {
+                "name": "Purple 80",
+                "color": "#412356"
+            },
+            {
+                "name": "Purple 90",
+                "color": "#311a41"
+            },
+            {
+                "name": "R=1 G=1 B=3",
+                "color": "#010103"
+            },
+            {
+                "name": "Teal 10",
+                "color": "#a7fae6"
+            },
+            {
+                "name": "Teal 20",
+                "color": "#6eedd8"
+            },
+            {
+                "name": "Teal 30",
+                "color": "#41d6c3"
+            },
+            {
+                "name": "Teal 40",
+                "color": "#00b4a0"
+            },
+            {
+                "name": "Teal 50",
+                "color": "#008571"
+            },
+            {
+                "name": "Teal 60",
+                "color": "#006d5d"
+            },
+            {
+                "name": "Teal 70",
+                "color": "#005448"
+            },
+            {
+                "name": "Teal 80",
+                "color": "#003c32"
+            },
+            {
+                "name": "Teal 90",
+                "color": "#012b22"
+            },
+            {
+                "name": "R=0 G=2 B=2",
+                "color": "#000202"
+            },
+            {
+                "name": "White",
+                "color": "#ffffff"
+            },
+            {
+                "name": "White 2",
+                "color": "#f9f9f9"
+            },
+            {
+                "name": "White 3",
+                "color": "#f4f4f4"
+            },
+            {
+                "name": "White 4",
+                "color": "#ececec"
+            },
+            {
+                "name": "Grey 10",
+                "color": "#e0e0e0"
+            },
+            {
+                "name": "Grey 20",
+                "color": "#c7c7c7"
+            },
+            {
+                "name": "Grey 30",
+                "color": "#aeaeae"
+            },
+            {
+                "name": "Grey 40",
+                "color": "#959595"
+            },
+            {
+                "name": "Grey 50",
+                "color": "#777677"
+            },
+            {
+                "name": "Grey 60",
+                "color": "#5a5a5a"
+            },
+            {
+                "name": "Grey 70",
+                "color": "#464646"
+            },
+            {
+                "name": "Grey 80",
+                "color": "#323232"
+            },
+            {
+                "name": "Grey 90",
+                "color": "#1e1e1e"
+            },
+            {
+                "name": "Grey 100",
+                "color": "#000000"
+            },
+            {
+                "name": "Grey 100 copy",
+                "color": "#000000"
+            }
+        ],
+        "name": "Purple-Teal"
+    },
+    {
+        "swatches": [
+            {
+                "color": "#eed2ff",
+                "name": "Purple 10"
+            },
+            {
+                "color": "#d7aaff",
+                "name": "Purple 20"
+            },
+            {
+                "color": "#ba8ff7",
+                "name": "Purple 30"
+            },
+            {
+                "color": "#af6ee8",
+                "name": "Purple 40"
+            },
+            {
+                "color": "#9855d4",
+                "name": "Purple 50"
+            },
+            {
+                "color": "#734098",
+                "name": "Purple 60"
+            },
+            {
+                "color": "#562f72",
+                "name": "Purple 70"
+            },
+            {
+                "color": "#412356",
+                "name": "Purple 80"
+            },
+            {
+                "color": "#311a41",
+                "name": "Purple 90"
+            },
+            {
+                "color": "#010103",
+                "name": "R=1 G=1 B=3"
+            },
+            {
+                "color": "#c0e6ff",
+                "name": "Blue 10"
+            },
+            {
+                "color": "#7cc7ff",
+                "name": "Blue 20"
+            },
+            {
+                "color": "#5aaafa",
+                "name": "Blue 30"
+            },
+            {
+                "color": "#5596e6",
+                "name": "Blue 40"
+            },
+            {
+                "color": "#4178be",
+                "name": "Blue 50"
+            },
+            {
+                "color": "#325c80",
+                "name": "Blue 60"
+            },
+            {
+                "color": "#264a60",
+                "name": "Blue 70"
+            },
+            {
+                "color": "#1d3649",
+                "name": "Blue 80"
+            },
+            {
+                "color": "#152935",
+                "name": "Blue 90"
+            },
+            {
+                "color": "#010205",
+                "name": "R=1 G=2 B=5"
+            },
+            {
+                "color": "#ffffff",
+                "name": "White"
+            },
+            {
+                "color": "#f9f9f9",
+                "name": "White 2"
+            },
+            {
+                "color": "#f4f4f4",
+                "name": "White 3"
+            },
+            {
+                "color": "#ececec",
+                "name": "White 4"
+            },
+            {
+                "color": "#e0e0e0",
+                "name": "Grey 10"
+            },
+            {
+                "color": "#c7c7c7",
+                "name": "Grey 20"
+            },
+            {
+                "color": "#aeaeae",
+                "name": "Grey 30"
+            },
+            {
+                "color": "#959595",
+                "name": "Grey 40"
+            },
+            {
+                "color": "#777677",
+                "name": "Grey 50"
+            },
+            {
+                "color": "#5a5a5a",
+                "name": "Grey 60"
+            },
+            {
+                "color": "#464646",
+                "name": "Grey 70"
+            },
+            {
+                "color": "#323232",
+                "name": "Grey 80"
+            },
+            {
+                "color": "#1e1e1e",
+                "name": "Grey 90"
+            },
+            {
+                "color": "#000000",
+                "name": "Grey 100"
+            },
+            {
+                "color": "#000000",
+                "name": "Grey 100 copy"
+            }
+        ],
+        "name": "Purple-Blue"
+    },
+    {
+        "swatches": [
+            {
+                "name": "Green 10",
+                "color": "#c8f08f"
+            },
+            {
+                "name": "Green 20",
+                "color": "#b4e051"
+            },
+            {
+                "name": "Green 30",
+                "color": "#8cd211"
+            },
+            {
+                "name": "Green 40",
+                "color": "#5aa700"
+            },
+            {
+                "name": "Green 50",
+                "color": "#4b8400"
+            },
+            {
+                "name": "Green 60",
+                "color": "#2d660a"
+            },
+            {
+                "name": "Green 70",
+                "color": "#144d14"
+            },
+            {
+                "name": "Green 80",
+                "color": "#0a3c02"
+            },
+            {
+                "name": "Green 90",
+                "color": "#0c2808"
+            },
+            {
+                "name": "R=1 G=2 B=0",
+                "color": "#010200"
+            },
+            {
+                "name": "Teal 10",
+                "color": "#a7fae6"
+            },
+            {
+                "name": "Teal 20",
+                "color": "#6eedd8"
+            },
+            {
+                "name": "Teal 30",
+                "color": "#41d6c3"
+            },
+            {
+                "name": "Teal 40",
+                "color": "#00b4a0"
+            },
+            {
+                "name": "Teal 50",
+                "color": "#008571"
+            },
+            {
+                "name": "Teal 60",
+                "color": "#006d5d"
+            },
+            {
+                "name": "Teal 70",
+                "color": "#005448"
+            },
+            {
+                "name": "Teal 80",
+                "color": "#003c32"
+            },
+            {
+                "name": "Teal 90",
+                "color": "#012b22"
+            },
+            {
+                "name": "R=0 G=2 B=2",
+                "color": "#000202"
+            },
+            {
+                "name": "White",
+                "color": "#ffffff"
+            },
+            {
+                "name": "White 2",
+                "color": "#f9f9f9"
+            },
+            {
+                "name": "White 3",
+                "color": "#f4f4f4"
+            },
+            {
+                "name": "White 4",
+                "color": "#ececec"
+            },
+            {
+                "name": "Grey 10",
+                "color": "#e0e0e0"
+            },
+            {
+                "name": "Grey 20",
+                "color": "#c7c7c7"
+            },
+            {
+                "name": "Grey 30",
+                "color": "#aeaeae"
+            },
+            {
+                "name": "Grey 40",
+                "color": "#959595"
+            },
+            {
+                "name": "Grey 50",
+                "color": "#777677"
+            },
+            {
+                "name": "Grey 60",
+                "color": "#5a5a5a"
+            },
+            {
+                "name": "Grey 70",
+                "color": "#464646"
+            },
+            {
+                "name": "Grey 80",
+                "color": "#323232"
+            },
+            {
+                "name": "Grey 90",
+                "color": "#1e1e1e"
+            },
+            {
+                "name": "Grey 100",
+                "color": "#000000"
+            },
+            {
+                "name": "Grey 100 copy",
+                "color": "#000000"
+            }
+        ],
+        "name": "Green-Teal"
+    },
+    {
+        "swatches": [
+            {
+                "color": "#c0e6ff",
+                "name": "Blue 10"
+            },
+            {
+                "color": "#7cc7ff",
+                "name": "Blue 20"
+            },
+            {
+                "color": "#5aaafa",
+                "name": "Blue 30"
+            },
+            {
+                "color": "#5596e6",
+                "name": "Blue 40"
+            },
+            {
+                "color": "#4178be",
+                "name": "Blue 50"
+            },
+            {
+                "color": "#325c80",
+                "name": "Blue 60"
+            },
+            {
+                "color": "#264a60",
+                "name": "Blue 70"
+            },
+            {
+                "color": "#1d3649",
+                "name": "Blue 80"
+            },
+            {
+                "color": "#152935",
+                "name": "Blue 90"
+            },
+            {
+                "color": "#010205",
+                "name": "R=1 G=2 B=5"
+            },
+            {
+                "color": "#a7fae6",
+                "name": "Teal 10"
+            },
+            {
+                "color": "#6eedd8",
+                "name": "Teal 20"
+            },
+            {
+                "color": "#41d6c3",
+                "name": "Teal 30"
+            },
+            {
+                "color": "#00b4a0",
+                "name": "Teal 40"
+            },
+            {
+                "color": "#008571",
+                "name": "Teal 50"
+            },
+            {
+                "color": "#006d5d",
+                "name": "Teal 60"
+            },
+            {
+                "color": "#005448",
+                "name": "Teal 70"
+            },
+            {
+                "color": "#003c32",
+                "name": "Teal 80"
+            },
+            {
+                "color": "#012b22",
+                "name": "Teal 90"
+            },
+            {
+                "color": "#000202",
+                "name": "R=0 G=2 B=2"
+            },
+            {
+                "color": "#ffffff",
+                "name": "White"
+            },
+            {
+                "color": "#f9f9f9",
+                "name": "White 2"
+            },
+            {
+                "color": "#f4f4f4",
+                "name": "White 3"
+            },
+            {
+                "color": "#ececec",
+                "name": "White 4"
+            },
+            {
+                "color": "#e0e0e0",
+                "name": "Grey 10"
+            },
+            {
+                "color": "#c7c7c7",
+                "name": "Grey 20"
+            },
+            {
+                "color": "#aeaeae",
+                "name": "Grey 30"
+            },
+            {
+                "color": "#959595",
+                "name": "Grey 40"
+            },
+            {
+                "color": "#777677",
+                "name": "Grey 50"
+            },
+            {
+                "color": "#5a5a5a",
+                "name": "Grey 60"
+            },
+            {
+                "color": "#464646",
+                "name": "Grey 70"
+            },
+            {
+                "color": "#323232",
+                "name": "Grey 80"
+            },
+            {
+                "color": "#1e1e1e",
+                "name": "Grey 90"
+            },
+            {
+                "color": "#000000",
+                "name": "Grey 100"
+            },
+            {
+                "color": "#000000",
+                "name": "Grey 100 copy"
+            }
+        ],
+        "name": "Blue-Teal"
+    }
+]
